5a16c562188b5064aa9570f4af837d57439b5e3a
Dhruvil <dkingg.8803@gmail.com>
2022-07-29 00:48:54 +0530

    make it better

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ var app = express()
 app.use(cors())
 app.use(express.json());
     app.use(express.static(path.resolve(__dirname, "./client/build")));
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname,"./client/build", "/index.html"))
-    })
 app.get("/test", (req,res)=>{
     res.send("Welcome");
 })
