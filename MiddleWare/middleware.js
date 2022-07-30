69b20841fa6fe9dca30ac84543699c9548cd3bb8
Dhruvil <dkingg.8803@gmail.com>
2022-07-30 18:29:09 +0530

    My commit

--- MiddleWare/middleware.js
+++ MiddleWare/middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const SECRET_KEY = process.env.SECRET_KEY;
 const middleware = (req, res, next)=>{
    if(!req.headers.authtoken) return res.send({
-      success: true,
+      success: false,
       message: "Login or signup first"
    })
    try{
